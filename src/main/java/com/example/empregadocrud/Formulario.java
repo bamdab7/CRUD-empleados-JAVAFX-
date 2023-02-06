575a91710b46d402324cfc21b691ae9dd89ddc21
nekoo <nereapena42gmail.com>
2023-02-06 09:50:09 +0100

    Creando interfaz basica

--- src/main/java/com/example/empregadocrud/Formulario.java
+++ src/main/java/com/example/empregadocrud/Formulario.java
@@ -11,7 +11,7 @@ public class Formulario extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Formulario.class.getResource("FormularioEmpleados.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
         stage.setTitle("Formulario registro Empleados");
         stage.setScene(scene);
         stage.show();
